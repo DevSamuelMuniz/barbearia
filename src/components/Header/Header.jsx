eaa293403119362dc0ecf021b577db7b8c04ed8d
Eudes Jordão <82156813+EudesJordao@users.noreply.github.com>
2024-07-26 18:09:13 -0300

    Header_Finish

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,28 +7,26 @@ function Header() {
     return (
         <main>
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
-                    <img src={Logo} alt="Logo" className="logoNav" />
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="/login">INICIO</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" href="/login" >AGENDA</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" href="/login" >FINANCEIRO</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link " aria-disabled="true" href="/login" >SAIR</a>
-                            </li>
-                        </ul>
+                <div className="container">
+                    <div class="container-fluid">
+                        <img src={Logo} alt="Logo" className="logoNav" />
+                        <div class=" navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="/home">INICIO</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link active" href="/login" >AGENDA</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link active" href="/login" >FINANCEIRO</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
+                </div>
+                <div className="exitNav">
+                    <a href="/" className="sairNav">Sair</a> 
                 </div>
             </nav>
         </main>
