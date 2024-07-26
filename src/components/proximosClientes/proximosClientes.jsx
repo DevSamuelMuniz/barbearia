b2f22ce97667501919bb86a0dae573388a528565
Samuel Muniz <137716288+DevSamuelMuniz@users.noreply.github.com>
2024-07-26 17:46:07 -0300

    card

--- src/components/proximosClientes/proximosClientes.jsx
+++ src/components/proximosClientes/proximosClientes.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 
+//components
+import CardPc from "../CardPc/CardPc";
+
 const date = '10/11/2024    '
 const filtro = 'Mais recentes'
 
 function ProximosClientes() {
-
+    const dados = {
+        nome: 'SAMUEL MUNIZ DA SILVA',
+        barbeiro: 'BARB. MATHEUS',
+        hora: '10:40',
+    };
+    
     return (
         <main>
             <div>
@@ -12,6 +20,14 @@ function ProximosClientes() {
 
                 <div>{date}</div>
                 <div>{filtro}</div>
+            </div>
+
+            <div>
+                <CardPc
+                    nome={dados.nome}
+                    barbeiro={dados.barbeiro}
+                    hora={dados.hora}
+                />
             </div>
         </main>
     )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,17 @@ import React from "react";
 
 //components
 import Header from "../../components/Header/Header";
+import ProximosClientes from "../../components/proximosClientes/proximosClientes";
 
 function Home() {
+  
+
     return (
         <main>
             <Header />
+            <ProximosClientes />
+
+            
         </main>
     )
 }
